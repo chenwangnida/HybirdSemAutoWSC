ad03fdd511714f9d53bb4bad4b0a7314a5f365d5
Jeffrey Wong <chenwang@Chens-MacBook-Pro.local>
2016-10-16 00:40:42 +1300

    update

---
+++ src/hybirdsem/owlssm/jade/ontology/SendStatusAction.java
@@ -0,0 +1,16 @@
+/*
+ * Created on 22.06.2006
+ */
+package hybirdsem.owlssm.jade.ontology;
+
+import jade.content.AgentAction;
+
+/**
+ * This class implements an agent action which causes the matchmaker agent to inform the
+ * requester about it's status.
+ *
+ * @author Patrick Kapahnke
+ */
+public class SendStatusAction implements AgentAction {
+
+}
