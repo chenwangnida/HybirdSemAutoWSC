07932af672276e8e2d9e5f48ab4c1823b68954af
Jeffrey Wong <chenwang@Chens-MacBook-Pro.local>
2016-10-18 23:02:55 +1300

    up

--- src/hybirdsem/owlssm/wsc/utils/MatchmakerInterface.java
+++ src/hybirdsem/owlssm/wsc/utils/MatchmakerInterface.java
@@ -1,22 +1,3 @@
-/*
- * Created on 13.10.2005
- * OWL-S Matchmaker
- * 
- * COPYRIGHT NOTICE
- * 
- * Copyright (C) 2005 DFKI GmbH, Germany
- * Developed by Benedikt Fries, Matthias Klusch
- * 
- * The code is free for non-commercial use only.
- * You can redistribute it and/or modify it under the terms
- * of the Mozilla Public License version 1.1  as
- * published by the Mozilla Foundation at
- * http://www.mozilla.org/MPL/MPL-1.1.txt
- */
-
-/**
- * 
- */
 package hybirdsem.owlssm.wsc.utils;
 
 import java.net.URI;
@@ -38,7 +19,7 @@ import hybirdsem.owlssm.similaritymeasures.JensenShannonMeasure;
 import hybirdsem.owlssm.wsc.utils.MatchMakerState;
 
 /**
- * @author Ben
+ * @author Chen Wang
  *
  */
 public class MatchmakerInterface {	
